aca53e7b756c6a996b01cf4266bc6abf956a64c6
Nicolas Roger <nramonteirods05@gmail.com>
2024-10-10 18:43:09 +0000

    database_introduction

---
+++ exemplos.py
@@ -0,0 +1,13 @@
+# .txt
+# banco de dados
+# SQ Lite
+
+# S-Q-L
+# Structured query language
+# Linguage de Consulta Estruturada
+
+# SELECT * FROM CLIENTES
+#nome, sobrenome, idade
+
+# ORM - esconde comandos SQL 
+# Para cada linguagem existe uma biblioteca ORM
